3b0552466c42e327c4e410a64dc102790ad05ca7
Ludger Visser <ludgerlef@hotmail.com>
2025-09-26 10:58:58 +0200

    updates

--- src/views/AllQuestionsView.tsx
+++ src/views/AllQuestionsView.tsx
@@ -1171,7 +1171,7 @@ async function generateFollowUps(
   setError(null);
   setLoading(true);
   try {
-    const res = await fetch('https://api.fynlo.nl/followups', {
+    const res = await fetch('https://api.fynlo.nl/followups-experimental', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ password, answers, instruction }),
